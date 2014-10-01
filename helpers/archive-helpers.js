aeca4b6f40279a49cb874d949400cce4b6f8ce46
Oleg24 <o.yanchinskiy@gmail.com>
2014-10-01 16:11:50 -0700

    passing first 2 tests

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
 
+
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
  * Consider using the `paths` object below to store frequently used file paths. This way,
@@ -26,6 +27,7 @@ exports.initialize = function(pathsObj){
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(){
+
 };
 
 exports.isUrlInList = function(){
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,42 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var request = require('request');
+var express = require('express')
+var fs = require('fs');
 // require more modules/folders here!
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10, // Seconds.
+  'Content-Type': 'application/json'
+};
+var options = {
+  encoding: 'utf8'
+}
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log('test');
+  if(req.method === "GET"){
+    if(req.url === '/'){
+      res.writeHead(200, headers);
+
+      fs.readFile(archive.paths.siteAssets+'/index.html', options, function(error, contents){
+        if(error) { throw error };
+        res.end(contents);
+      })
+    } else if(req.url === "/www.google.com"){
+        res.writeHead(200, headers);
+
+        fs.readFile(archive.paths.archivedSites+"/www.google.com", options, function(error, contents){
+          if(error) { throw error }
+          res.end(contents);
+        })
+    }
+    console.log("req method is "+req.method);
+  }
+  if(req.method === "POST"){
+    //fs.writeFile(archive.paths.list, function)
+  }
 };
+
